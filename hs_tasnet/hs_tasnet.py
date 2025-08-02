9fe7470ef1c57cd27aa59b65cfecc99701362d61
lucidrains <lucidrains@gmail.com>
2025-08-02 07:34:14 -0700

    start with the "memory" lstm

--- hs_tasnet/hs_tasnet.py
+++ hs_tasnet/hs_tasnet.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import torch
-from torch.nn import Module, ModuleList
+from torch import Tensor, tensor, is_tensor
+from torch.nn import LSTM, Module, ModuleList
 
 # functions
 
@@ -8,6 +11,54 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+# "memory lstm"
+# as described in section 3.3, it is just two lstm layers with identity skip connection
+
+class MemoryLSTM(Module):
+    def __init__(
+        self,
+        dim,
+        dim_inner = None
+    ):
+        super().__init__()
+        dim_inner = default(dim_inner, dim)
+
+        self.lstm1 = LSTM(dim, dim_inner, batch_first = True)
+        self.lstm2 = LSTM(dim_inner, dim, batch_first = True)
+
+    def forward(
+        self,
+        feats,
+        hiddens: tuple[Tensor, Tensor] | None = None
+    ) -> tuple[Tensor, tuple[Tensor, Tensor]]:
+
+        # handle previous hiddens
+
+        hidden1 = hidden2 = None
+
+        if exists(hiddens):
+            hidden1, hidden2 = hiddens
+
+        # store residual
+
+        residual = feats
+
+        # the two lstms
+
+        inner, next_hidden1 = self.lstm1(feats, hidden1)
+
+        out, next_hidden2 = self.lstm2(inner, hidden2)
+
+        # add the residual
+
+        out_with_residual = out + residual
+
+        # pass out the next hiddens as tuple of two tensors
+
+        next_hiddens = (next_hidden1, next_hidden2)
+
+        return out_with_residual, next_hiddens
 
 # classes
 
--- tests/test_hs_tasnet.py
+++ tests/test_hs_tasnet.py
@@ -1,9 +1,21 @@
 import pytest
 
 import torch
-from hs_tasnet.hs_tasnet import HSTasNet
+
+def test_memory_lstm():
+    from hs_tasnet.hs_tasnet import MemoryLSTM
+
+    memory_lstm = MemoryLSTM(512, 1024)
+
+    feats = torch.randn(3, 1024, 512)
+
+    out1, hiddens1 = memory_lstm(feats)
+    out2, hiddens2 = memory_lstm(feats, hiddens1)
+
+    assert out1.shape == out2.shape == feats.shape
 
 def test_model():
+    from hs_tasnet.hs_tasnet import HSTasNet
     model = HSTasNet(512)
 
     audio = torch.randn(1, 8192 * 2)
