ad75e68e065cb08fd78c6b5e3d712632840352b1
lucidrains <lucidrains@gmail.com>
2025-08-04 06:39:11 -0700

    residuals for default version, make small configurable some other time

--- hs_tasnet/hs_tasnet.py
+++ hs_tasnet/hs_tasnet.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from functools import partial
+from functools import partial, wraps
 
 import torch
 from torch import Tensor, tensor, is_tensor, cat
@@ -17,13 +17,24 @@ def exists(v):
 def default(v, d):
     return v if exists(v) else d
 
+# residual
+
+def residual(fn):
+
+    @wraps(fn)
+    def decorated(t, *args, **kwargs):
+        out, hidden = fn(t, *args, **kwargs)
+        return t + out, hidden
+
+    return decorated
+
 # classes
 
 class HSTasNet(Module):
     def __init__(
         self,
-        dim = 500,      # they have 500 hidden units for the network, with 1000 at fusion (concat from both representation branches)
-        small = False   # params cut in half by 1 layer lstm vs 2, fusion uses summed representation
+        dim = 500,       # they have 500 hidden units for the network, with 1000 at fusion (concat from both representation branches)
+        small = False,   # params cut in half by 1 layer lstm vs 2, fusion uses summed representation
     ):
         super().__init__()
 
@@ -70,9 +81,9 @@ class HSTasNet(Module):
 
         spec_residual, waveform_residual = spec, waveform
 
-        spec, next_pre_spec_hidden = self.pre_spec_branch(spec, pre_spec_hidden)
+        spec, next_pre_spec_hidden = residual(self.pre_spec_branch)(spec, pre_spec_hidden)
 
-        waveform, next_pre_waveform_hidden = self.pre_waveform_branch(waveform, pre_waveform_hidden)
+        waveform, next_pre_waveform_hidden = residual(self.pre_waveform_branch)(waveform, pre_waveform_hidden)
 
         # if small, they just sum the two branches
 
@@ -83,7 +94,7 @@ class HSTasNet(Module):
 
         # fusing
 
-        fused, next_fusion_hidden = self.fusion_branch(fusion_input, fusion_hidden)
+        fused, next_fusion_hidden = residual(self.fusion_branch)(fusion_input, fusion_hidden)
 
         # split if not small, handle small next week
 
@@ -100,9 +111,9 @@ class HSTasNet(Module):
 
         # layer for both branches
 
-        spec, next_post_spec_hidden = self.post_spec_branch(spec, post_spec_hidden)
+        spec, next_post_spec_hidden = residual(self.post_spec_branch)(spec, post_spec_hidden)
 
-        waveform, next_post_waveform_hidden = self.post_waveform_branch(waveform, post_waveform_hidden)
+        waveform, next_post_waveform_hidden = residual(self.post_waveform_branch)(waveform, post_waveform_hidden)
 
         # outputs
 
